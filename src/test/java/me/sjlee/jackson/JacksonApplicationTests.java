f327682bf66443b458e9c5226804f6b75e4a9809
we <robin00q@naver.com>
2020-10-17 16:24:30 +0900

    first commit

---
+++ src/test/java/me/sjlee/jackson/JacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sjlee.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
